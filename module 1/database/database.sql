9ec718e5d741c30df313f83b885244818ba51f55
Sergey <pmswga@gmail.com>
2024-03-10 11:35:01 +0300

    - сверстали страницы

--- module 1/database/database.sql
+++ module 1/database/database.sql
@@ -13,6 +13,17 @@ insert into claim_status (caption) values
 ('подтверждено'),
 ('отклонено');
 
+
+CREATE TABLE user_types (
+    id INT PRIMARY KEY auto_increment,
+    type_name VARCHAR(50) not null
+);
+
+insert into user_types (type_name) values
+('обычный'),
+('админ');
+
+
 create table users (
 	id int primary key auto_increment,
     second_name varchar(50) not null,
@@ -21,11 +32,14 @@ create table users (
     tel varchar(12) not null,
     email varchar(50) not null unique,
     login varchar(20) not null,
-    password varchar(255) not null
+    password varchar(255) not null,
+    type_id int not null,
+    foreign key (type_id) references user_types (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-insert into users (second_name, first_name, tel, email, login, password) values
-('Test', 'Test', '+79543214566', 'test', 'test@mail.ru', 'password');
+insert into users (second_name, first_name, tel, email, login, password, type_id) values
+('Test', 'Test', '+79543214566', 'test', 'test@mail.ru', 'password', 1),
+('Admin', 'Admin', '+77777778899', 'admin', 'admin@admin.ru', 'password', 2);
 
 
 create table claims (
